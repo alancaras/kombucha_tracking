c288a95679183404ec36ae6a269c82cb8f21bb2f
alancaras <alancaras@gmail.com>
2019-05-23 11:10:46 +1000

    fix_submitii

--- javascript/kombucha.js
+++ javascript/kombucha.js
@@ -1,15 +1,1 @@
-// on click of sbumit it should take whatever is inside the submit box
-// need an empty array in the beginning
-// delete item
 
-let listArray = ["tomato", "banana"];
-
-let Item;
-
-function addItem() {
-  Item = document.getElementById("enter").value;
-  console.log(Item);
-  listArray.push(Item);
-  console.log(listArray);
-  document.getElementById("grocerylist").value = listArray;
-}
